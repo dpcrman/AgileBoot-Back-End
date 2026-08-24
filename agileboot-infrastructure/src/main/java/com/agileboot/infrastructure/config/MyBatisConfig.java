174a5a7387a3d428566e1dc461cc3bbca6066fae
qinhaichuan <haichuan@lykj.com>
2026-08-24 22:15:02 +0800

    feat: add PostgreSQL support with optimized MyBatis mappers and configuration

--- agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/config/MyBatisConfig.java
+++ agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/config/MyBatisConfig.java
@@ -22,7 +22,7 @@ public class MyBatisConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return interceptor;
     }
